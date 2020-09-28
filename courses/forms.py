889eb9221deac6e006c9ab9b6f623eabb0b5109d
Glenn Velupillai <glennsvelu@gmail.com>
2020-09-28 14:33:01 -0400

    Update forms.py

--- courses/forms.py
+++ courses/forms.py
@@ -9,14 +9,6 @@ class QuizForm(forms.ModelForm):
 class QuestionForm(forms.ModelForm):
     """ The form for a question """
     
-    class Media:
-        css = {'all':('course/css/order.css',)}
-        js = (
-            'courses/js/vendor/jquery.fn.sortable.min.js',
-            'courses/js/order.js'
-        )
-
-
 class TrueFalseQuestionForm(QuestionForm):
     """ The form for a true/false question """
     
